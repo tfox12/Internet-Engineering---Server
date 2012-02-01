55814a45375b693f51f895398d438a26a9096894
unknown <Administrator@.(none)>
2012-02-01 12:15:56 -0500

    fixed errors in client header

--- client/socketlayer.h
+++ client/socketlayer.h
@@ -1,14 +1,8 @@
 #ifndef _CLIENT_SOCKETLAYER_
 #define _CLIENT_SOCKETLAYER_
 int
-connect_beta(const char [], const char[]);
+connect_host(const char [], const char[]);
 
-int
-connect_ip(const char [], const char[]);
-
-
-int
-connect_dns(const char [], const char[]);
 
 
 #endif
