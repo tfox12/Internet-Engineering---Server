dd68b2764044aad121f89c803553b5a3147e5320
Fox McCloud <tfox12@kent.edu>
2012-01-31 19:55:00 -0500

    File Manager is for the most part done... it was really simple. It does need to be changed eventually to check if absolute path blah blah parser shit i dont give a FUCK about

--- filemanager.c
+++ filemanager.c
@@ -1,10 +1,21 @@
 #include "filemanager.h"
 #include "config.h"
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
-(void *)
+char *
 get_contents_of_file(char * filename)
 {
+    FILE * requested_file = fopen(filename,"r");
+    
+    fseek (requested_file , 0 , SEEK_END);
+    int filesize = ftell (requested_file);
+    
+    rewind (requested_file);
 
+    char * data = (char *) calloc(filesize,sizeof(char));
+
+    fread(data,sizeof(char),filesize,requested_file);
+
+    return data; /* data must be deallocated by caller */
 }
