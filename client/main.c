e8e6b5f15dafe7d20a045964814e0bb04f60b02e
unknown <adam@mrMime.(none)>
2012-02-13 11:42:03 -0500

    functionality there is not - making good progress towards URL parsing

--- client/main.c
+++ client/main.c
@@ -23,7 +23,33 @@ getHttpGetTarget(char* target)
 {
 	int index = 0;
 	char targetHost[1000];
+	char* temp;
+	temp = strtok(target, "://");
+	
+	while(temp != NULL)
+	{	
+		char *newLine;
+		newLine = strchr(temp, '\n');
+		
+		if(newLine != NULL)
+		{
+			*newLine = '\0';
+			memset(newLine, 0, sizeof(newLine));
+		}
+			
+		if(index == 1)
+		{
+			
+		}
+	
+		printf("%s : index %d \n", temp, index);
+		temp = strtok(NULL, "/");
+		index++;
+		
 
+	}
+	
+	
 	while(*target != '\0')
 	{
 		if(*target == '/')
@@ -36,16 +62,22 @@ getHttpGetTarget(char* target)
 				target++;
 			
 			}
-			break;
+			
 			targetHost[index] = '\0';
+			break;
 		}
 			
 		++target;	
 	}
+	
+	
 	if(index == 0)
 		printf("%d", 1);
+		
+		
 	char *targetFile = index == 0 ? "/" : &targetHost;
 
+	printf("%s %s", targetFile, "yo mama");
 	return targetFile;
 
 }
@@ -57,7 +89,31 @@ getHttpGetTarget(char* target)
 void
 formatHost(char* input)
 {
-	input = strtok (input,"/");
+	char *temp;
+	temp = strtok(input, "://");
+	int index = 0;
+	
+	while(temp != NULL)
+	{
+		if(index == 3)
+			break;
+		//printf("%s%d\n", temp, index);
+		if(index == 1)
+		{
+			printf("%d", 10);
+			printf("%s %s", temp, "Debug");
+			strcpy(input,temp);
+			//printf("%s", input);
+			/*char *newline = strchr(input, '\n');
+			*newline = '\0';*/
+		}
+		temp = strtok(NULL, "//");
+		index++;
+	}
+	
+	/*temp = strtok(input, "://");
+	input = strtok(input,"/");*/
+	puts("yup");
 }
 
 
@@ -85,6 +141,8 @@ main(int argCount, char **arguments)
 		sprintf(msg, "GET %s HTTP/1.0\r\nHost: www.lol.biz\r\nUser-Agent: My HTTP\r\n\r\n",getHttpGetTarget(targetHost));
 		formatHost(targetHost);
 		len = strlen(msg);
+		printf("%s", msg);
+		printf("%s", targetHost);
         connectionSocket =  connect_host(targetHost, port);
         bytes_sent = send(connectionSocket, msg, len, 0);
         ret = recv(connectionSocket, response, sizeof(response), 0);
